1d88717785c7bc7c8164ea933d78cc853a5f2324
Tessa Mero <tmero@Tessas-MacBook-Pro.local>
2019-04-17 20:44:37 -0700

    update main page

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,28 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        seattlevuejs
+        Seattle Vue JS Meetup
       </h1>
       <h2 class="subtitle">
-        Seattle Vue JS Meetup Website
+        Welcome to our new website!
       </h2>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
+          href="https://meetup.com/seattlevuejs"
           target="_blank"
-          class="button--green">Documentation</a>
+          class="button--green">Meetup</a>
         <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://github.com/seattlevuejs"
           target="_blank"
           class="button--grey">GitHub</a>
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          class="button--green">Twitter</a>
+        <a 
+        	href="https://join.slack.com/t/seattlevuejs/shared_invite/enQtNjAwNTEwNzg2Njc1LTVjNzFjOWE1ZDdjZTFkMzg1YjAwNDc3MGFkMzZlMDE0ZmNhYzRlNGU5ZTMzNWVmYjA1NWNjYjU0OTQyMmE1NmM"
+        	target="_blank"
+        	class="button--grey">Slack</a>
       </div>
     </div>
   </section>
@@ -39,6 +47,8 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  background-color: #fefbf7;
+ /* background-image: url("https://res.cloudinary.com/tessamero/image/upload/c_crop,h_623,w_1920/v1555557754/seattle.jpg");*/
 }
 
 .title {
